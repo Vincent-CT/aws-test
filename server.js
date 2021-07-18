1244289c805e3d00e579fd4bf600b4432ec832d4
Vincent Tran <vince1@live.dk>
2021-07-18 19:24:39 +0200

    add dotenv

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
