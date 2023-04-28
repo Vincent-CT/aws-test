281aba3da3f7e8e98918e3fd67fa654a6b517b8a
Vincent Tran <44893882+Vincent-CT@users.noreply.github.com>
2023-04-28 15:10:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
